5f441ec8c9e6f493c876d2e89d15bffd6f47ad4a
Vinod Supnekar <vinod.supnekar@playerzpot.com>
2020-05-16 16:23:22 +0530

    singleton added for HTTPClient

--- EssentialFeedTests/ReoteFeedLoaderTests.swift
+++ EssentialFeedTests/ReoteFeedLoaderTests.swift
@@ -12,31 +12,36 @@ class ReoteFeedLoaderTests: XCTestCase {
   
   class RemoteFeedLoader {
     func load() {
-    
+      HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
     }
     
   }
 
   class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {
+    
+    }
     var requestedURL: URL?
   }
   
   //Commit:- remote feed loader does not request data on creation
   func test_init_doesNotRequestDataFromURL() {
     _ = RemoteFeedLoader()
-      let client = HTTPClient()
+    let client = HTTPClient.shared
       XCTAssertNil(client.requestedURL)
   }
  
-  //Use case:- Load feed items
-//  func test_load_requestDataFromURL() {
-//    let client = HTTPClient()
-//    let sut = RemoteFeedLoader()
-//
-//    sut.load()
-//
-//    //When we sut.load(), then we we will havd client with requestedURL
-//    XCTAssertNotNil(client.requestedURL)
-//  }
+ // Use case:- Load feed items
+  func test_load_requestDataFromURL() {
+    let client = HTTPClient.shared
+    let sut = RemoteFeedLoader()
+
+    sut.load()
+
+    //When we sut.load(), then we we will havd client with requestedURL
+    XCTAssertNotNil(client.requestedURL)
+  }
 
 }
