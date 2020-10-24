ea6fd6ccb058c4f9a39a89020c42ab999281ae1c
vinodsupnekar <vinod.supnekar@gmail.com>
2020-10-24 16:09:10 +0530

    code formatting

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -34,9 +34,9 @@ class LocalFeedLoader {
 
 protocol FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
-    func deleteCacheFeed(completion : @escaping DeletionCompletion)
-    
     typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCacheFeed(completion : @escaping DeletionCompletion)
     func insert(_ items: [FeedItem],timestamp: Date,completion : @escaping InsertionCompletion)
     
 }
