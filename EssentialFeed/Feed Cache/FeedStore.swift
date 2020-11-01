78b385d5ca1aed2ccee359862db1bfcf9915b06d
vinodsupnekar <vinod.supnekar@gmail.com>
2020-11-01 18:00:16 +0530

    Move FeedStore to its own file

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,18 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by vinod supnekar on 01/11/20.
+//  Copyright © 2020 VinodS. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCacheFeed(completion : @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem],timestamp: Date,completion : @escaping InsertionCompletion)
+    
+}
