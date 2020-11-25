7cc849dd6fc99223b7f678952853b7453e14934b
vinodsupnekar <vinod.supnekar@gmail.com>
2020-11-25 17:04:41 +0530

    load command delivers no images on seven days old cache

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -21,17 +21,23 @@ public final class LocalFeedLoader {
     }
 
     public func load(completion: @escaping (LoadResult?) -> Void) {
-        store.retrieve { result  in
+        store.retrieve { [unowned self] result  in
             switch result {
                 case let .failure(error) :
                     completion(.failure(error))
-            case .empty:
-                    completion(.success([]))
-             
-            case let .found(feed,_):
+                case let .found(feed,timestamp) where   self.validate(timestamp):
                 completion(.success(feed.toModel()))
+                case  .found,.empty:
+                    completion(.success([]))
             }
         }
+    }
+    
+    private func validate(_ timestamp: Date) -> Bool {
+        let calender = Calendar(identifier: .gregorian)
+        guard let maxCacheAge = calender.date(byAdding: .day, value: 7, to: timestamp) else { return false }
+        
+        return currentDate() < maxCacheAge
     }
     
     public func save(_ feed:[FeedImage], completion: @escaping (SaveResult) -> Void) {
--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -44,12 +44,23 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     
     func test_load_deliversCachedImagesOnLessThanSevenDaysOldCache() {
         let feed = uniqueImageFeed()
-        let (sut,store) = makeSUT()
-        let currentDate = Date()
-        let lessThanSevenDaysOldTimestamp = currentDate.adding(days: -7).adding(seconds: 1)
-        
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut,store) = makeSUT(currentDate: {fixedCurrentDate})
+
         expect(sut, toCompleteWith: .success(feed.models)) {
             store.completeRetrieval(with: feed.local,timestamp: lessThanSevenDaysOldTimestamp)
+        }
+    }
+    
+    func test_load_deliversNoImagesOnSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let sevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
+        let (sut,store) = makeSUT(currentDate: {fixedCurrentDate})
+
+        expect(sut, toCompleteWith: .success([])) {
+            store.completeRetrieval(with: feed.local,timestamp: sevenDaysOldTimestamp)
         }
     }
     
