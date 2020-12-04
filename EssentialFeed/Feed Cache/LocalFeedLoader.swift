df22fcadb0f5fdef9fe4935b09719178ece54fa2
vinodsupnekar <vinod.supnekar@gmail.com>
2020-12-04 13:19:48 +0530

    unowned reference test

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -19,6 +19,10 @@ public final class LocalFeedLoader {
     public init(store: FeedStore, currentDate: @escaping ()  -> Date ) {
         self.store = store
         self.currentDate = currentDate
+        let cls = TestUnownedVariables()
+//        cls.testCode()
+//        cls.testUnownedRef()
+        cls.testUnOwnedRefWithImplicitlyUnwrappedOptional()
     }
 
     public func load(completion: @escaping (LoadResult?) -> Void) {
@@ -81,6 +85,93 @@ private extension Array where Element == LocalFeedImage {
     }
 }
 
+private class Department {
+    var name: String
+    var courses: [Course]
+    init(name: String) {
+        self.name = name
+        self.courses = []
+    }
+    deinit { print("Department #\(self.name) is being deinitialized") }
+}
+
+private class Course {
+    var name: String
+    unowned var department: Department
+    unowned var nextCourse: Course?
+    init(name: String, in department: Department) {
+        self.name = name
+        self.department = department
+        self.nextCourse = nil
+    }
+    deinit { print("Course #\(self.name) is being deinitialized") }
+}
+
+private class TestUnownedVariables {
+    
+    func testCode() {
+        var department: Department? = Department(name: "Horticulture")
+        let intro = Course(name: "Survey of Plants", in: department!)
+        let intermediate = Course(name: "Growing Common Herbs", in: department!)
+        let advanced = Course(name: "Caring for Tropical Plants", in: department!)
+        print("intro next course is \(intro.nextCourse)")
+            intro.nextCourse = intermediate
+            intermediate.nextCourse = advanced
+        department!.courses = [intro, intermediate, advanced]
+        
+//        department.courses = []
+        department = nil
+    }
+    
+    func testUnownedRef() {
+        var john: Customer? = Customer(name: "John Appleseed")
+        john!.card = CreditCard(number: 1234_5678_9012_3456, customer: john!)
+    
+        john = nil
+    }
+    
+    func testUnOwnedRefWithImplicitlyUnwrappedOptional() {
+            let country = Country(name: "Canada", capitalName: "Ottawa")
+            print("\(country.name)'s capital city is called \(country.capitalCity.name)")
+    }
+    
+}
+
+class Customer {
+    var name: String
+    var card: CreditCard?
+    init(name: String) {
+        self.name = name
+    }
+    deinit { print("\(name) is being deinitialized") }
+}
+
+class CreditCard {
+    let number: UInt64
+    unowned let customer: Customer
+    init(number: UInt64, customer: Customer) {
+        self.number = number
+        self.customer = customer
+    }
+    deinit { print("Card #\(number) is being deinitialized") }
+}
 
 
+class Country {
+    let name: String
+    var capitalCity: City!
+    init(name: String, capitalName: String) {
+        self.name = name
+        self.capitalCity = City(name: capitalName, country: self)
+    }
+}
 
+class City {
+    let name: String
+    unowned let country: Country
+    init(name: String, country: Country) {
+        self.name = name
+        self.country = country
+    }
+    
+}
