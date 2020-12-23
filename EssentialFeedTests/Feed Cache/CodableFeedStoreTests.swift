3516c6018948cabac4e6eba4f70b259b98711127
vinodsupnekar <vinod.supnekar@gmail.com>
2020-12-23 13:23:20 +0530

    Retrieve delivers failure on retrieval error (invalid cached data)

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -50,9 +50,14 @@ class CodableFeedStore {
        guard let data = try? Data(contentsOf: storeURL) else {
         return completion(.empty)
         }
+        
+        do {
         let decoder = JSONDecoder()
-        let cache = try! decoder.decode(Cache.self, from: data)
+        let cache = try decoder.decode(Cache.self, from: data)
         completion(.found(feed: cache.feed.map { $0.local }, timestamp: cache.timestamp))
+        } catch {
+            completion(.failure(error))
+        }
     }
     
     func insert(_ feed: [LocalFeedImage],timestamp: Date,completion : @escaping FeedStore.InsertionCompletion) {
@@ -106,6 +111,14 @@ class CodableFeedStoreTests: XCTestCase {
         
         expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp))
     }
+    
+    func test_retrieve_deliversFailureOnRetrievalError() {
+        let sut = makeSUT()
+        
+        try! "invalid data".write(to: testSpecificStoreURL(), atomically: false, encoding: .utf8)
+        
+        expect(sut, toRetrieve: .failure(anyNSError()))
+    }
 
     // - MARK: Helpers
     
@@ -136,14 +149,15 @@ class CodableFeedStoreTests: XCTestCase {
         
         sut.retrieve { retrievedResult in
             switch (expectedResult, retrievedResult) {
-                case (.empty,.empty):
+            case (.empty,.empty),(.failure, .failure):
                   break
-            case let (.found(expected), .found(retrived)):
-                XCTAssertEqual(retrived.feed, expected.feed, file: file, line: line)
-                XCTAssertEqual(retrived.timestamp, expected.timestamp, file: file, line: line)
-            default:
-                 XCTFail("expected to retrieve \(expectedResult), got \(retrievedResult) instead",file: file, line: line)
-            }
+                    
+                case let (.found(expected), .found(retrived)):
+                    XCTAssertEqual(retrived.feed, expected.feed, file: file, line: line)
+                    XCTAssertEqual(retrived.timestamp, expected.timestamp, file: file, line: line)
+                default:
+                    XCTFail("expected to retrieve \(expectedResult), got \(retrievedResult) instead",file: file, line: line)
+                }
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
