606dd25454a53ac525f20eb9d540ff8b1fd4d35b
vinodsupnekar <vinod.supnekar@gmail.com>
2021-03-19 15:41:18 +0530

    Simply 'CoreDataFeedStore' completion code with the new 'Result' API's

--- EssentialFeed/Feed Cache/Infrastructure/Core data/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/Infrastructure/Core data/CoreDataFeedStore.swift
@@ -40,50 +40,39 @@ public final class CoreDataFeedStore: FeedStore {
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
         perform { context in
-            do {
-                
-                if let cache = try ManagedCache.find(in: context) {
-                    completion(.success( .some(CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp))))
+            
+            completion(Result{
+                try ManagedCache.find(in: context).map {
+                    return CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
                 }
-                else {
-                    completion(.success(.none))
-                }
-                
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         
         perform { context in
-            do {
+            completion(Result {
                 let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timestamp
                 managedCache.feed =  ManagedFeedImage.images(from: feed, in: context)
                 try context.save()
-                completion(.success(Void()))
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
     
     public func deleteCacheFeed(completion: @escaping DeletionCompletion) {
 
         perform { context in
-            do {
+            
+            completion(Result {
                 try ManagedCache.find(in: context).map { (obj) -> Void in
                     context.delete(obj)
                 }.map({
                     try context.save()
                 })
-                completion(.success(Void()))
-            }
-            catch {
-                completion(.failure(error))
-            }
+            })
+
         }
     }
     
