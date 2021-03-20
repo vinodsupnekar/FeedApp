70c3aece0515745c742dee48867b3ee64cf6247b
vinodsupnekar <vinod.supnekar@gmail.com>
2021-03-20 22:54:56 +0530

    Add 'FeeImageCell' with storyboard connection

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,28 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by vinod supnekar on 20/03/21.
+//
+
+import UIKit
+
+class FeedImageCell: UITableViewCell {
+    
+    @IBOutlet private(set) var locationContainer: UIView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
