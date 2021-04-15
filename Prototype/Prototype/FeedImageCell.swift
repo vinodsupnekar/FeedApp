8ab8b22f296944f8ff1bfcccb67c1b4a24e5ace6
vinodsupnekar <vinod.supnekar@gmail.com>
2021-04-15 10:27:59 +0530

    Add FeedImageCell with storyboard outlets

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by vinod supnekar on 14/04/21.
+//
+
+import UIKit
+
+class FeedImageCell: UITableViewCell {
+    
+    @IBOutlet private(set) var locationContainer: UIView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var descritpionLabel: UILabel!
+
+
+}
