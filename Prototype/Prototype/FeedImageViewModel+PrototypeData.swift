59127129d82a243f42b9d22f5f51e945defcd771
vinodsupnekar <vinod.supnekar@gmail.com>
2021-04-15 10:57:12 +0530

    Render prototype feed data on the tableview

--- Prototype/Prototype/FeedImageViewModel+PrototypeData.swift
+++ Prototype/Prototype/FeedImageViewModel+PrototypeData.swift
@@ -9,6 +9,6 @@ import Foundation
 
 extension FeedImageViewModel {
     static var prototypeFeed: [FeedImageViewModel] {
-        return [FeedImageViewModel(description: "asasas asas asasas xcxcv sdfsdf dsffdf df fgfgfgfg dfdf sdsdsdsdsd we w w wqwqwqwqw qwqwqwqwqw ww www wqwqw qw qw qwqw qwqwqwqwqwqw", location: "asasas asasas", imageName: "image-0"),FeedImageViewModel(description: nil, location: "asasas", imageName: "image-1"),FeedImageViewModel(description: "asasas asas  qwqwqwqwqw ww www wqwqw qw qw qwqw qwqwqwqwqwqw", location: "asasas", imageName: "image-2"),FeedImageViewModel(description: nil, location: "asasas", imageName: "image-3")]
+        return [FeedImageViewModel(description: "asasas asas asasas xcxcv sdfsdf dsffdf df fgfgfgfg dfdf sdsdsdsdsd we w w wqwqwqwqw qwqwqwqwqw ww www wqwqw qw qw qwqw qwqwqwqwqwqw", location: "asasas asasas", imageName: "image-0"),FeedImageViewModel(description: nil, location: "asasas", imageName: "image-1"),FeedImageViewModel(description: "asasas asas  qwqwqwqwqw ww www wqwqw qw qw qwqw qwqwqwqwqwqw", location: "asasas", imageName: "image-2"),FeedImageViewModel(description: "asasas asas asasas xcxcv sdfsdf dsffdf df fgfgfgfg dfdf sdsdsdsdsd we w w wqwqwqwqw qwqwqwqwqw ww www wqwqw qw qw qwqw qwqwqwqwqwqw qwertyuiop asdfghjkl zxcvbnm", location: "asasas", imageName: "image-3")]
     }
 }
